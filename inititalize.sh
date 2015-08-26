f375a440030cb3beba4c9ac387d49c2b8ea08c92
ffilippov <f.filippov7@gmail.com>
2015-08-26 15:17:08 +0300

    updated init

--- inititalize.sh
+++ inititalize.sh
@@ -1,19 +1,47 @@
 #!/bin/bash
 
-if [ -f ~/.bashrc ]
-then
-  mv ~/.bashrc ~/.bashrc.orig &&
-  ln -s ~/rc.files/.bashrc ~/.bashrc &&
-  . ~/.bashrc
-        else
-          ln -s ~/rc.files/.bashrc ~/.bashrc &&
-          . ~/.bashrc
-fi
+CURUSER=$USER
 
-if [ -f ~/.vimrc ]
-then
-  mv ~/.vimrc ~/.vimrc.orig &&
-  ln -s ~/rc.files/.vimrc ~/.vimrc &&
-        else
-          ln -s ~/rc.files/.vimrc ~/.vimrc
-fi
+case $CURUSER in
+	root)
+  if [ -f ~/.bashrc ]
+  then
+    mv ~/.bashrc ~/.bashrc.orig &&
+    ln -s ~/rc.files/root.bashrc ~/.bashrc &&
+    . ~/.bashrc
+          else
+	    rm ~/.bashrc
+            ln -s ~/rc.files/root.bashrc ~/.bashrc &&
+            . ~/.bashrc
+  fi
+  if [ -f ~/.vimrc ]
+  then
+    mv ~/.vimrc ~/.vimrc.orig &&
+    ln -s ~/rc.files/.vimrc ~/.vimrc
+          else
+	    rm ~/.vimrc
+            ln -s ~/rc.files/.vimrc ~/.vimrc
+  fi;;
+	[a-zA-Z0-9]*)
+  if [ -f ~/.bashrc ]
+  then
+    mv ~/.bashrc ~/.bashrc.orig &&
+    ln -s ~/rc.files/user.bashrc ~/.bashrc &&
+    . ~/.bashrc
+          else
+	    rm ~/.bashrc
+            ln -s ~/rc.files/user.bashrc ~/.bashrc &&
+            . ~/.bashrc
+  fi
+  if [ -f ~/.vimrc ]
+  then
+    mv ~/.vimrc ~/.vimrc.orig &&
+    ln -s ~/rc.files/.vimrc ~/.vimrc
+          else
+	    rm ~/.vimrc
+            ln -s ~/rc.files/.vimrc ~/.vimrc
+  fi;;
+esac
+
+echo "Bash version is: $(bash --version | grep release | sed -r 's/.*version\ (.*)\(.*\)-release.*/\1/')"
+
